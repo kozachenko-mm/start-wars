8bd14c808e4ecb4f28133e827f36181f399f2d92
Michael Kozachenko <Kozachenko.Michael@gmail.com>
2020-02-07 17:03:14 +0200

    Updates

---
+++ precache-manifest.20a01203a96fd474589392358d5b0af4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e982be407e9af7b536d4d57cbc44943d",
+    "url": "/start-wars/index.html"
+  },
+  {
+    "revision": "0a6f631934efd89f3945",
+    "url": "/start-wars/static/css/main.ac544b26.chunk.css"
+  },
+  {
+    "revision": "a1dce2e682c3fbd5895a",
+    "url": "/start-wars/static/js/2.8a40dbbb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/start-wars/static/js/2.8a40dbbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6f631934efd89f3945",
+    "url": "/start-wars/static/js/main.49d337f9.chunk.js"
+  },
+  {
+    "revision": "0a006e66e50bd97f90fa",
+    "url": "/start-wars/static/js/runtime-main.6047ff9b.js"
+  }
+]);
